b36505bb85229f5fef095318bb1c72a406774c5f
Giulian Biolo <giulianbiolo@gmail.com>
2023-12-07 00:27:49 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> Result<(), Error> {
     stepper.set_direction(Direction::Forward)?;
     
     // Travel speed is 1.0 m / s -> 3 rev / s, final_speed = 0.0 m / s, distance is 2.5 meters
-    loop{stepper.travel_ease_in_out(&spin_sleep, 25.0, 15.0, 0.0);}
+    loop{stepper.travel_ease_in_out(&spin_sleep, 2.0, 15.0, 0.0);}
 
     stepper.disable()?;
     Ok(())
