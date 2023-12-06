62bda2e5be27108e24dcbaaff27da2f42daf2e49
Giulian Biolo <giulianbiolo@gmail.com>
2023-12-06 23:43:24 +0100

    WIP Driver Motors

---
+++ src/drv8825.rs
@@ -0,0 +1,102 @@
+#![allow(dead_code)]
+use rppal::gpio::{Gpio, Error, OutputPin};
+use spin_sleep::SpinSleeper;
+
+const ONE_MILLIS: std::time::Duration = std::time::Duration::from_millis(1);
+
+pub enum Direction { Forward, Reverse }
+pub struct DRV8825 {
+    enable: Option<OutputPin>,
+    step: OutputPin,
+    dir: OutputPin,
+    microsteps: u8,
+    radius: f64,
+    accel: f64,
+    cur_speed: f64,
+}
+
+impl DRV8825 {
+    pub fn new(gpio: &Gpio, enable_pin: Option<u8>, step_pin: u8, dir_pin: u8, microsteps: u8, radius: f64, accel: f64) -> Result<DRV8825, Error> {
+        if let Some(enable_pin) = enable_pin {
+            let enable: OutputPin = gpio.get(enable_pin)?.into_output();
+            let step: OutputPin = gpio.get(step_pin)?.into_output();
+            let dir: OutputPin = gpio.get(dir_pin)?.into_output();
+            Ok(DRV8825 { enable: Some(enable), step, dir, microsteps, radius, accel, cur_speed: 0.0 })
+        } else {
+            let step: OutputPin = gpio.get(step_pin)?.into_output();
+            let dir: OutputPin = gpio.get(dir_pin)?.into_output();
+            Ok(DRV8825 { enable: None, step, dir, microsteps, radius, accel, cur_speed: 0.0 })
+        }
+    }
+    pub fn enable(&mut self) -> Result<(), Error> {
+        if let Some(enable) = &mut self.enable { enable.set_low(); }
+        Ok(())
+    }
+    pub fn disable(&mut self) -> Result<(), Error> {
+        if let Some(enable) = &mut self.enable { enable.set_high(); }
+        Ok(())
+    }
+    pub fn set_direction(&mut self, direction: Direction) -> Result<(), Error> {
+        match direction {
+            Direction::Forward => self.dir.set_high(),
+            Direction::Reverse => self.dir.set_low(),
+        }
+        Ok(())
+    }
+    pub fn step(&mut self, spin_sleep: &SpinSleeper, timing_ns: u64) {
+        self.step.set_high();
+        spin_sleep.sleep_ns(timing_ns / 2);
+        self.step.set_low();
+        spin_sleep.sleep_ns(timing_ns / 2);
+    }
+    pub fn step_n(&mut self, spin_sleep: &SpinSleeper, n: u32) {
+        for _ in 0..n { self.step(spin_sleep, ONE_MILLIS.as_nanos() as u64); }
+    }
+    // * steps_from_distance(distance) returns the number of steps required to move the given distance.
+    fn steps_from_distance(&self, distance: f64) -> u32 {
+        let circumference: f64 = 2.0 * std::f64::consts::PI * self.radius;
+        let steps: f64 = (distance / circumference) * (self.microsteps as f64);
+        steps as u32
+    }
+    // * get_timing() returns the number of nanoseconds to wait between steps for the current speed.
+    fn get_timing(&self) -> u64 { (1_000_000_000 / self.steps_from_distance(self.cur_speed).clamp(10, u32::MAX)) as u64 }
+
+    // * travel(spin_sleep, distance, target_speed) moves the motor the given distance through the given target_speed.
+    pub fn travel_ease_in(&mut self, spin_sleep: &SpinSleeper, distance: f64, travel_speed: f64) {
+        // we want to spool up or down the motors from cur_speed to target_speed, reaching target_speed at min time (max accel) and at the end of the travel keep target_speed
+        let accel_decel: f64 = (travel_speed - self.cur_speed).signum() as f64; // 1 for accel, -1 for decel
+        for _ in 0..self.steps_from_distance(distance) {
+            if (accel_decel > 0.0 && self.cur_speed < travel_speed) || (accel_decel < 0.0 && self.cur_speed > travel_speed) {
+                self.cur_speed += self.accel * accel_decel;
+            }
+            self.step(spin_sleep, self.get_timing());
+        }
+    }
+    pub fn travel_ease_in_out(&mut self, spin_sleep: &SpinSleeper, distance: f64, travel_speed: f64, final_speed: f64) {
+        // we want to spool up or down the motors from cur_speed to target_speed, reaching target_speed at min time (max accel) and at the end must come to a stop at min time (-max accel)
+        let accel_decel: f64 = (travel_speed - self.cur_speed).signum() as f64; // 1 for accel, -1 for decel
+        let steps: u32 = self.steps_from_distance(distance);
+        let mut reached_idx: u32 = steps;
+        for idx in 0..steps {
+            // compute if we need to start decelerating
+            if (accel_decel > 0.0 && self.cur_speed < travel_speed) || (accel_decel < 0.0 && self.cur_speed > travel_speed) {
+                self.cur_speed += self.accel * accel_decel;
+            }
+            self.step(spin_sleep, self.get_timing());
+            let steps_to_final_speed: u32 = ((self.cur_speed - final_speed).abs() / self.accel) as u32;
+            if steps_to_final_speed >= steps - idx {
+                reached_idx = idx;
+                break;
+            }
+            // delta speed / acceleration == step to final speed
+            // if step to stop < step remaining in for loop (self.steps_from_distance(distance) - i) then we need to start decelerating
+        }
+        let accel_decel: f64 = (final_speed - self.cur_speed).signum() as f64; // 1 for accel, -1 for decel
+        for _ in reached_idx..steps {
+            if (accel_decel > 0.0 && self.cur_speed < final_speed) || (accel_decel < 0.0 && self.cur_speed > final_speed) {
+                self.cur_speed += self.accel * accel_decel;
+            }
+            self.step(spin_sleep, self.get_timing());
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use rppal::gpio::{Gpio, Error};
+use spin_sleep::SpinSleeper;
+
+mod drv8825;
+use drv8825::{DRV8825, Direction};
+
+// const DRV8825_ENABLE_PIN: u8 = 17; // BCM 17 = Physical 11, Enable Pin
+const DRV8825_STEP_PIN: u8 = 27; // BCM 27 = Physical 13, Step Pin
+const DRV8825_DIR_PIN: u8 = 22; // BCM 22 = Physical 15, Direction Pin
+const MICROSTEPS: u8 = 1;
+const RADIUS: f64 = 0.05;
+const ACCEL: f64 = 0.01;
+
+fn main() -> Result<(), Error> {
+    let gpio: Gpio = Gpio::new()?;
+    let spin_sleep: SpinSleeper = SpinSleeper::default();
+    let mut stepper: DRV8825 = DRV8825::new(&gpio, None, DRV8825_STEP_PIN, DRV8825_DIR_PIN, MICROSTEPS, RADIUS, ACCEL)?;
+    println!("Hello, world!");
+    stepper.enable()?;
+    stepper.set_direction(Direction::Forward)?;
+    
+    //stepper.travel_ease_in(&spin_sleep, 2.5, 0.1);
+    spin_sleep.sleep_s(2.0);
+
+    stepper.disable()?;
+    Ok(())
+}
